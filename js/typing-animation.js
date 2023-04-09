c597fcc8e47cbac7f1c4611a8f84f4e1ffe2f8a1
kevinsouza4 <kevin008152@gmail.com>
2023-04-09 08:48:04 -0300

    adicionando novo projeto e organizando js

--- js/typing-animation.js
+++ js/typing-animation.js
@@ -41,18 +41,4 @@ const initTypingAnimation = () => {
     setTimeout(() => typingAnimation(paragraph), 3700)
   
   }
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-         if (entry.isIntersecting) {
-             entry.target.classList.add('show');
-         } else {
-             entry.target.classList.remove('show');
-         }
-    });
-  });
-
-  const hiddenElements = document.querySelectorAll('.hidden');
-  hiddenElements.forEach((el) => observer.observe(el));
-
-
 initTypingAnimation()
